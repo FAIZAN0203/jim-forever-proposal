a0ba7cc85e52c63d71c16bc08565097330ad28de
FAIZAN0203 <immortalsurfer786@gmail.com>
2026-02-07 23:33:01 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "PASTE_FROM_FIREBASE",
+  authDomain: "PASTE_FROM_FIREBASE",
+  projectId: "PASTE_FROM_FIREBASE",
+  storageBucket: "PASTE_FROM_FIREBASE",
+  messagingSenderId: "PASTE_FROM_FIREBASE",
+  appId: "PASTE_FROM_FIREBASE"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
